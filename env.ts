0a45c58653e901dbcf0e9798a01e554b63babf2d
loremus <loremus@proton.me>
2026-07-14 15:46:15 +0530

    testing docker environment with fallbacks for env.ts that support it

--- env.ts
+++ env.ts
@@ -3,9 +3,9 @@ import * as z from "zod";
 
 export const env = createEnv({
     server: {
-        BETTER_AUTH_SECRET: z.base64().length(32),
-        BETTER_AUTH_URL: z.url(),
-        DATABASE_URL: z.url()
+        BETTER_AUTH_SECRET: z.base64().length(32).default('KoWndF16ygEvCKiEv5iLrKauH8X3cYIr'),
+        BETTER_AUTH_URL: z.url().default('http://localhost:3000'),
+        DATABASE_URL: z.url().default('postgres://testing:queer@localhost:5432/testing')
     },
     runtimeEnv: process.env,
     emptyStringAsUndefined: true,
