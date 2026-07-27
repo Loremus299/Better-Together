275f5a8c51d47140adf12397ccce5bf3df5d3f9b
loremus <loremus@proton.me>
2026-07-27 18:30:56 +0530

    some service rework

--- app/api/habit/service.ts
+++ app/api/habit/service.ts
@@ -1,191 +1,158 @@
 import { db } from "@/db";
-import { user } from "@/db/schema";
-import {
-  habitMembersTable,
-  habitTable,
-  habitTasksTable,
-} from "@/db/schemas/habit";
+import { habitMembersTable, habitTable, habitTasksTable } from "@/db/schema";
 import { log } from "@/lib/evlog";
 import { isError, isOk, Result } from "@/lib/result";
-import { createId } from "@paralleldrive/cuid2";
-import { and, eq, InferSelectModel } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 
-async function createNewHabit({
-  name,
-  description,
-  userId
+async function isUserAdmin({
+  userId,
+  habitId,
+  tx = db,
 }: {
-  name: string;
-  description: string;
-  userId: string
-}): Promise<Result<string, string>> {
-  const id = createId();
-
+  userId: string;
+  habitId: string;
+  tx: typeof db;
+}): Promise<Result<boolean, string>> {
   try {
-    await db.transaction(async (tx) => {
-      await tx
-        .insert(habitTable)
-        .values({ id, name, description, admin: userId });
-      await tx
-        .insert(habitMembersTable)
-        .values({ habit: id, member: userId });
-      await tx
-        .insert(habitTasksTable)
-        .values({
-          habit: id,
-          task: "Add members to hold yourself accountable.",
-        });
-      await tx
-        .insert(habitTasksTable)
-        .values({ habit: id, task: "Create tasks for habit." });
-    });
+    const habitById = await tx
+      .select()
+      .from(habitTable)
+      .where(eq(habitTable.id, habitId));
 
-    return isOk(id);
-  } catch (e) {
-    log.error("500", e as string);
-    return isError("Could not create habit.");
+    if (habitById.length == 0) {
+      log.error("404", "Habit Not Found");
+      isError("Habit not found.");
+    }
+
+    if (habitById[0].admin == userId) {
+      log.info("status", "is admin");
+      return isOk(true);
+    } else {
+      log.info("status", "is not admin");
+      return isOk(false);
+    }
+  } catch (error) {
+    log.error("500", error as string);
+    return isError("Could not verify user authority over the habit as admin.");
   }
 }
 
-async function editHabit({ habit, description, name, userId }: { habit: string, description: string, name: string, userId: string }) {
-  const getHabit = await db.select().from(habitTable).where(eq(habitTable.id, habit))
-
-  if (getHabit.length == 0) {
-    log.error("404", "Habit not found")
-    return isError("No habit found")
-  }
-
-  if (getHabit[0].admin !== userId) {
-    log.error("401", "User cannot edit habit")
-    return isError("Only admins can edit habits.")
-  }
-
+async function isUserMember({
+  userId,
+  habitId,
+  tx = db,
+}: {
+  userId: string;
+  habitId: string;
+  tx: typeof db;
+}): Promise<Result<boolean, string>> {
   try {
-    await db.update(habitTable).set({ description, name }).where(eq(habitTable.id, habit))
-    return isOk(null)
-  } catch (e) {
-    log.error("500", e as string)
-    return isError("Could not edit habit.")
-  }
-}
-
-async function getHabits({ userId }: { userId: string }): Promise<Result<InferSelectModel<typeof habitTable>[], string>> {
-  try {
-    const habitDetails = await db
-      .select({
-        id: habitTable.id,
-        name: habitTable.name,
-        description: habitTable.description,
-        admin: habitTable.admin,
-        createdAt: habitTable.createdAt,
-        updatedAt: habitTable.updatedAt,
-      })
+    const isMember = await tx
+      .select()
       .from(habitMembersTable)
-      .innerJoin(habitTable, eq(habitTable.id, habitMembersTable.habit))
-      .where(eq(habitMembersTable.member, userId));
+      .where(
+        and(
+          eq(habitMembersTable.habit, habitId),
+          eq(habitMembersTable.member, userId),
+        ),
+      );
 
-    return isOk(habitDetails);
-  } catch (e) {
-    log.error("500", e as string);
-    return isError("Could not fetch habits for user.")
+    if (isMember.length == 0) {
+      log.info("status", "is member");
+      return isOk(false);
+    } else {
+      log.info("status", "is not member");
+      return isOk(true);
+    }
+  } catch (error) {
+    log.error("500", error as string);
+    return isError(
+      "Could not verify authority of the user over the habit as member.",
+    );
   }
 }
 
-async function addMemberToHabit({
-  habit,
-  memberEmail,
-  userId
+async function createTaskInHabit({
+  userId,
+  taskName,
+  taskDescription,
+  habitId,
+  tx = db,
 }: {
-  habit: string;
-  memberEmail: string;
-  userId: string
-}): Promise<Result<null, string>> {
-  const checkAuthority = await db
-    .select({ admin: habitTable.admin })
-    .from(habitTable)
-    .where(eq(habitTable.id, habit));
-  if (!(checkAuthority[0].admin == userId)) {
-    log.error("400", "User doesn't have authority to add members.");
-    return isError("Only admins can add members.");
+  userId: string;
+  taskName: string;
+  taskDescription: string;
+  habitId: string;
+  tx: typeof db;
+}): Promise<Result<string, string>> {
+  const isAdmin = await isUserAdmin({ userId, habitId, tx });
+  if (!isAdmin.success) {
+    return isError(isAdmin.error);
   }
-
-  const validateMember = await db
-    .select({ id: user.id })
-    .from(user)
-    .where(eq(user.email, memberEmail));
-  if (validateMember.length == 0) {
-    log.error("404", "User doesn't exist");
-    return isError("Couldn't find user with that email.");
+  if (!isAdmin.data) {
+    log.error("403", "Forbidden");
+    return isError("Only admins can add tasks.");
   }
 
   try {
-    await db
-      .insert(habitMembersTable)
-      .values({ habit, member: validateMember[0].id });
-    return isOk(null);
-  } catch (e) {
-    log.error("500", e as string);
-    return isError("Could not add member.");
+    const taskId = await tx
+      .insert(habitTasksTable)
+      .values({
+        task: taskName,
+        description: taskDescription,
+        habit: habitId,
+      })
+      .returning({ id: habitTasksTable.id });
+
+    log.info("Task Id", taskId[0].id);
+    return isOk(taskId[0].id);
+  } catch (error) {
+    log.error("500", error as string);
+    return isError("Could not create task.");
   }
 }
 
-async function removeMemberFromHabit({ member, habit, userId }: { member: string, habit: string, userId: string }) {
-  const getHabit = await db.select().from(habitTable).where(eq(habitTable.id, habit))
-
-  if (getHabit.length == 0) {
-    log.error("404", "Habit not found")
-    return isError("No habit found")
-  }
-
-  if (getHabit[0].admin !== userId) {
-    log.error("401", "User cannot remove members.")
-    return isError("Only admins can remove members.")
-  }
-
-  try {
-    await db.delete(habitMembersTable).where(and(
-      eq(habitMembersTable.habit, habit),
-      eq(habitMembersTable.member, member)
-    ))
-    return isOk(null)
-  } catch (e) {
-    log.error("500", e as string)
-    return isError("Could not add member")
-  }
-}
-
-async function addTaskToHabit({
-  habit,
-  task,
-  userId
+async function updateTaskInHabit({
+  userId,
+  habitId,
+  taskId,
+  taskName,
+  taskDescription,
+  tx = db,
 }: {
-  habit: string;
-  task: string;
-  userId: string
-}): Promise<Result<null, string>> {
-  const checkAuthority = await db
-    .select({ admin: habitTable.admin })
-    .from(habitTable)
-    .where(eq(habitTable.id, habit));
-  if (!(checkAuthority[0].admin == userId)) {
-    log.error("400", "User doesn't have authority to add tasks");
-    return isError("Only Admins can add tasks.");
+  userId: string;
+  habitId: string;
+  taskId: string;
+  taskName: string;
+  taskDescription: string;
+  tx: typeof db;
+}) {
+  const isAdmin = await isUserAdmin({ userId, habitId, tx });
+  if (!isAdmin.success) {
+    return isError(isAdmin.error);
+  }
+  if (!isAdmin.data) {
+    log.error("403", "Forbidden");
+    return isError("Only admins can add tasks.");
   }
 
   try {
-    await db.insert(habitTasksTable).values({ habit, task });
-    return isOk(null);
-  } catch (e) {
-    log.error("500", e as string);
-    return isError("Could not add task.");
+    await tx
+      .update(habitTasksTable)
+      .set({
+        task: taskName,
+        description: taskDescription,
+      })
+      .where(eq(habitTasksTable.id, taskId));
+  } catch (error) {
+    log.error("500", error as string);
+    return isError("Could not edit task.");
   }
 }
 
 export const habitService = {
-  createNewHabit,
-  editHabit,
-  getHabits,
-  addMemberToHabit,
-  removeMemberFromHabit,
-  addTaskToHabit,
+  isUserAdmin,
+  isUserMember,
+  createTaskInHabit,
+  updateTaskInHabit,
 };
--- db/schemas/habit.ts
+++ db/schemas/habit.ts
@@ -7,6 +7,7 @@ export const habitTable = pgTable("habit", {
   id: text("id").primaryKey().$defaultFn(() => createId()),
   name: text("name").notNull(),
   description: text("description").notNull(),
+  header: text("header").references(() => mediaTable.id, { onDelete: "cascade" }).notNull(),
   admin: text("admin").references(() => user.id, { onDelete: "restrict" }).notNull(),
   createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
   updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow().$onUpdate(() => new Date()),
@@ -21,7 +22,10 @@ export const habitMembersTable = pgTable("habitMembers", {
 export const habitTasksTable = pgTable("habitTasks", {
   id: text("id").primaryKey().$defaultFn(() => createId()),
   habit: text("habit").references(() => habitTable.id, { onDelete: "cascade" }).notNull(),
-  task: text("task").notNull()
+  task: text("task").notNull(),
+  description: text("description").notNull(),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow().$onUpdate(() => new Date()),
 })
 
 export const proofStatusEnum = pgEnum("proofStatus", ["pending", "accepted", "declined"])
@@ -32,6 +36,7 @@ export const habitProofsTable = pgTable("habitProofs", {
   timeStamp: text("timestamp").$defaultFn(() => (Math.floor(Date.now() / 1000).toString())),
   description: text("description"),
   media: text("media").references(() => mediaTable.id, { onDelete: "cascade" }).notNull(),
-  proofStatus: proofStatusEnum("proofStatus").notNull()
+  proofStatus: proofStatusEnum("proofStatus").notNull(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow().$onUpdate(() => new Date()),
 })
 
