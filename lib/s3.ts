6337ef68f95122f26780c857731c82464f7037a4
loremus <loremus@proton.me>
2026-08-03 14:22:14 +0530

    s3

--- lib/s3.ts
+++ lib/s3.ts
@@ -2,10 +2,11 @@ import { env } from "@/env";
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-    region: "queer",
-    endpoint: 'https://localhost:9000',
-    credentials: {
-        accessKeyId: env.S3_ACCESSKEYID,
-        secretAccessKey: env.S3_SECRETACCESSKEY
-    }
-})
+  region: "us-east-1",
+  endpoint: "http://localhost:9000",
+  forcePathStyle: true,
+  credentials: {
+    accessKeyId: env.S3_ACCESSKEYID,
+    secretAccessKey: env.S3_SECRETACCESSKEY,
+  },
+});
