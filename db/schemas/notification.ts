4a2562a1a40a5123271159537b055f9fc32b98fb
loremus <loremus@proton.me>
2026-08-06 19:14:55 +0530

    woops

--- db/schemas/notification.ts
+++ db/schemas/notification.ts
@@ -1,10 +1,14 @@
 import { createId } from "@paralleldrive/cuid2";
 import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { user } from "./auth-schema";
 
 export const notificationTable = pgTable("notification", {
   id: text("id")
     .primaryKey()
     .$defaultFn(() => createId()),
+  user: text("user")
+    .references(() => user.id, { onDelete: "cascade" })
+    .notNull(),
   title: text("title").notNull(),
   description: text("description").notNull(),
   read: boolean("read").notNull().default(false),
