dab282bd5e26e81e7b548035b78086a246ec848a
loremus <loremus@proton.me>
2026-08-07 17:39:57 +0530

    notifications crud

--- app/api/notification/service.ts
+++ app/api/notification/service.ts
@@ -3,6 +3,7 @@ import { notificationTable } from "@/db/schema";
 import { log } from "@/lib/evlog";
 import { isError, isOk, Result } from "@/lib/result";
 import { DB } from "@/lib/utils";
+import { eq, InferSelectModel } from "drizzle-orm";
 
 async function createNotification({
   user,
@@ -28,6 +29,114 @@ async function createNotification({
   }
 }
 
+async function readNotificationById({
+  id,
+  user,
+  tx = db,
+}: {
+  id: string;
+  user: string;
+  tx: DB;
+}): Promise<Result<InferSelectModel<typeof notificationTable>, string>> {
+  const notif = await tx
+    .select()
+    .from(notificationTable)
+    .where(eq(notificationTable.id, id));
+
+  if (notif.length) {
+    log.error("404", "Could not find notif by id");
+    return isError("Notification not found.");
+  }
+
+  if ((notif[0].user = user)) {
+    log.error("403", "Forbidden");
+    return isError("You don't have access to this notification.");
+  }
+
+  return isOk(notif[0]);
+}
+
+async function readNotificationByUser({
+  user,
+  tx,
+}: {
+  user: string;
+  tx: DB;
+}): Promise<Result<InferSelectModel<typeof notificationTable>[], string>> {
+  try {
+    return isOk(
+      await tx
+        .select()
+        .from(notificationTable)
+        .where(eq(notificationTable.user, user)),
+    );
+  } catch (error) {
+    log.error("500", error as string);
+    return isError("Failed to get notifications for user");
+  }
+}
+
+async function updateNotification({
+  id,
+  title,
+  body,
+  read,
+  user,
+  tx,
+}: {
+  id: string;
+  title: string;
+  body: string;
+  read: boolean;
+  user: string;
+  tx: DB;
+}): Promise<Result<null, string>> {
+  const notif = await readNotificationById({ id, tx, user });
+
+  if (!notif.success) {
+    return isError(notif.error);
+  }
+
+  try {
+    await tx
+      .update(notificationTable)
+      .set({ read, title, body })
+      .where(eq(notificationTable.id, id));
+    return isOk(null);
+  } catch (error) {
+    log.error("500", error as string);
+    return isError("Could not update notification.");
+  }
+}
+
+async function deleteNotification({
+  id,
+  user,
+  tx,
+}: {
+  id: string;
+  user: string;
+  tx: DB;
+}): Promise<Result<null, string>> {
+  const notif = await readNotificationById({ id, tx, user });
+
+  if (!notif.success) {
+    return isError(notif.error);
+  }
+
+  try {
+    await tx.delete(notificationTable).where(eq(notificationTable.id, id));
+    return isOk(null);
+  } catch (error) {
+    log.error("500", error as string);
+    return isError("Could not delete notification.");
+  }
+}
+
 export const notificationService = {
   createNotification,
+  readNotificationById,
+  readNotificationByUser,
+  updateNotification,
+  deleteNotification,
 };
