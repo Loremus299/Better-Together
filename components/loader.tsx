7c15f24f8e6d91b3394bdc26302d9b49b2c15ce6
loremus <loremus@proton.me>
2026-08-11 18:18:04 +0530

    removed some ui stuff

--- components/loader.tsx
+++
@@ -1,5 +0,0 @@
-import { Suspense } from "react";
-
-export default function Loader({ children }: { children: React.ReactNode }) {
-  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
-}
