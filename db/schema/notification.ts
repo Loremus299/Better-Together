5f1e42d0be9e2855c83e841a5691aa1bfe1eb409
loremus <loremus@proton.me>
2026-08-12 14:32:08 +0530

    old notifs schema

---
+++ db/schema/notification.ts
@@ -0,0 +1,16 @@
+import { createId } from "@paralleldrive/cuid2";
+import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { user } from "./auth-schema";
+
+export const notificationTable = pgTable("notification", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  user: text("user")
+    .references(() => user.id, { onDelete: "cascade" })
+    .notNull(),
+  title: text("title").notNull(),
+  body: text("body").notNull(),
+  read: boolean("read").notNull().default(false),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+});
