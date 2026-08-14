ae21acc3964ab3ef2ce512922c7978c8a8a2bc00
loremus <loremus@proton.me>
2026-08-14 12:48:17 +0530

    habit page

--- app/(habit)/[habit]/page.tsx
+++ app/(habit)/[habit]/page.tsx
@@ -1,5 +1,12 @@
 import { habitService } from "@/app/api/habit/service";
 import { habitOps } from "@/app/api/ops/habit";
+import ImageById from "@/components/imageById";
+import MaxWContainer from "@/components/maxWContainer";
+import {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+} from "@/components/ui/resizable";
 import { Logger } from "@/lib/logger";
 import { getSession } from "@/lib/server-util";
 import { redirect } from "next/navigation";
@@ -48,5 +55,63 @@ export default async function Page({
 
   log.print();
 
-  return <div>Hi</div>;
+  return (
+    <MaxWContainer>
+      <main className="border border-muted-background">
+        <ResizablePanelGroup orientation="vertical" className="min-h-screen">
+          <ResizablePanel defaultSize={"120px"}>
+            {isAdmin.value.data ? (
+              <ResizablePanelGroup>
+                <ResizablePanel>
+                  <div className="w-full h-full p-4 flex gap-4">
+                    {habitdata.value.data.header && (
+                      <ImageById
+                        id={habitdata.value.data.header}
+                        css="h-full rounded-lg border hover:drop-shadow-2xl transition duration-300 hover:scale-105"
+                      />
+                    )}
+                    <div>
+                      <h1 className="text-4xl font-bold tracking-tight">
+                        {habitdata.value.data.name}
+                      </h1>
+                      <h2 className="text-muted-foreground">
+                        {habitdata.value.data.description}
+                      </h2>
+                    </div>
+                  </div>
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel defaultSize={"80px"}></ResizablePanel>
+              </ResizablePanelGroup>
+            ) : (
+              <div className="w-full h-full p-4 flex gap-4">
+                {habitdata.value.data.header && (
+                  <ImageById
+                    id={habitdata.value.data.header}
+                    css="h-full rounded-lg border hover:drop-shadow-2xl transition duration-300 hover:scale-105"
+                  />
+                )}
+                <div>
+                  <h1 className="text-5xl font-bold tracking-tight">
+                    {habitdata.value.data.name}
+                  </h1>
+                  <h2 className="text-muted-foreground">
+                    {habitdata.value.data.description}
+                  </h2>
+                </div>
+              </div>
+            )}
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel>
+            <ResizablePanelGroup>
+              <ResizablePanel defaultSize={"75%"}></ResizablePanel>
+              <ResizableHandle withHandle />
+              <ResizablePanel>Hi</ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </main>
+    </MaxWContainer>
+  );
 }
