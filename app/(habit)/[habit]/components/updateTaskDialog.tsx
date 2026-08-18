1a0d4ab463d4afe1fc0ea06de25bdcdae43a001c
loremus <loremus@proton.me>
2026-08-18 18:01:35 +0530

    update task ui

---
+++ app/(habit)/[habit]/components/updateTaskDialog.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { updateTaskAction } from "../action";
+import { toast } from "sonner";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { IconEdit } from "@tabler/icons-react";
+import FormController from "@/components/formController";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+const updateTaskSchema = z.object({
+  id: z.string().min(1),
+  task: z.string().min(1),
+  description: z.string().min(1),
+});
+type UpdateTaskValues = z.infer<typeof updateTaskSchema>;
+
+export default function UpdateTaskDialog({
+  id,
+  task,
+  description,
+}: {
+  id: string;
+  task: string;
+  description: string;
+}) {
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(updateTaskSchema),
+    defaultValues: {
+      id,
+      task,
+      description,
+    },
+  });
+
+  const onSubmit = async (values: UpdateTaskValues) => {
+    const act = await updateTaskAction(values);
+    if (!act.success) {
+      toast.error(act.error);
+    } else {
+      router.refresh();
+      toast.success("Modified task successfully.");
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <IconEdit className="size-4" />
+      </DialogTrigger>
+      <DialogContent>
+        <DialogTitle>Edit Task</DialogTitle>
+        <form className="grid gap-2" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormController
+            form={form}
+            label="New Task Name"
+            name="task"
+            placeholder=""
+            render={({ field, fieldState }) => (
+              <Input
+                {...field}
+                id={field.name}
+                aria-invalid={fieldState.invalid}
+              />
+            )}
+          />
+          <FormController
+            form={form}
+            label="New Task Name"
+            name="description"
+            placeholder=""
+            render={({ field, fieldState }) => (
+              <Textarea
+                {...field}
+                id={field.name}
+                aria-invalid={fieldState.invalid}
+              />
+            )}
+          />
+          <Button type="submit">
+            <IconEdit />
+            Update task
+          </Button>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
