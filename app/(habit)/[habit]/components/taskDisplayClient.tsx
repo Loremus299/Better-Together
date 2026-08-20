782e126dfc59a62ebcb5fc90bb99093a54bfbe34
loremus <loremus@proton.me>
2026-08-20 09:30:34 +0530

    ok I think this one is best

--- app/(habit)/[habit]/components/taskDisplayClient.tsx
+++ app/(habit)/[habit]/components/taskDisplayClient.tsx
@@ -18,7 +18,9 @@ export default function TaskDisplayClient({
         proofDate.getMonth() === today.getMonth() &&
         proofDate.getDate() === today.getDate();
 
-      return isFromToday && proof.proofStatus == "accepted";
+      return (
+        isFromToday && ["accepted", "declined"].includes(proof.proofStatus)
+      );
     });
 
   if (isDoneToday) {
